ecdae6faccd7be9fd9c0324efe48117ff1ae867b
Lou Huang <lou@louhuang.com>
2016-05-06 20:06:06 -0400

    Make data script better

--- data.sh
+++ data.sh
@@ -6,19 +6,21 @@ TEMP_DIR="./temp"
 TARGET_DIR="./site/data"
 TOPOJSON="./node_modules/.bin/topojson"
 
+# Make sure the `topojson' command is installed.
+if [ ! -e $TOPOJSON ]; then
+  echo "local topojson command not found, please run npm install first"
+  exit 1
+fi
+
 # Make the temporary download / processing directory
 mkdir -p temp
 
 # Get the source district boundary geojson
-# curl $DISTRICTS_GEO > "${TEMP_DIR}/districts.geojson"
+curl $DISTRICTS_GEO > $TEMP_DIR/districts.geojson
 
-# Convert to TopoJSON. Make sure the command is installed.
-if [ -e ${TOPOJSON} ]; then
-  # GeoJSON property 'BoroCD' is promoted to the object id in TopoJSON
-  # Slightly more precise quantization than default
-  # But we simplify the points, because there's a lot of coastline
-  # TODO: Test for optimum quantization / simplication parameters
-  ${TOPOJSON} -o ${TARGET_DIR}/districts.topojson --id-property BoroCD -q 1e6 -s 3e-12 ${TEMP_DIR}/districts.geojson
-else
-  echo "local topojson command not found, please run npm install first"
-fi
+# Convert to TopoJSON.
+# GeoJSON property 'BoroCD' is promoted to the object id in TopoJSON
+# Slightly more precise quantization than default
+# But we simplify the points, because there's a lot of coastline
+# TODO: Test for optimum quantization / simplication parameters
+$TOPOJSON -o $TARGET_DIR/districts.topojson --id-property BoroCD -q 1e6 -s 3e-12 $TEMP_DIR/districts.geojson
